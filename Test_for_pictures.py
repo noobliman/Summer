456f49977f1ec4d3157e983bcc717929a195ff12
Liman <rahman.rahmanliman.liman@gmail.com>
2018-06-25 16:19:49 +0530

    Update for testing downloaded files

---
+++ Test_for_pictures.py
@@ -0,0 +1,19 @@
+import numpy as np
+import cv2
+import os
+
+face_cascade = cv2.CascadeClassifier('/opt/opencv/data/haarcascades/haarcascade_frontalface_default.xml')
+pat=input("Enter path of picture to be tested with file extention\n")
+x=os.path.join(pat)
+imageSource = x
+img = cv2.imread(imageSource)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+for (x, y, w, h) in faces:
+    cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 255), 2)
+cv2.imshow('Video', img)
+while True:
+    k=cv2.waitKey(1)
+    if(k==27):
+    	break
+cv2.destroyAllWindows()
